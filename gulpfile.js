d5b716ef851a823bd5f3af3cfc592d882cca595d
yoshi1125hisa <yoppy1125yoppy@gmail.com>
2020-05-23 03:16:06 +0900

    Add new LP

--- gulpfile.js
+++ gulpfile.js
@@ -4,38 +4,38 @@ const sass = require('gulp-sass');
 const postcss = require('gulp-postcss');
 
 gulp.task('scss', () => {
-  const sass = require('gulp-sass')
-  const cssnext = require('postcss-cssnext')
-  const postcss = require('gulp-postcss')
-  const processors = [cssnext({
-    browsers: ['last 2 version']
-  })]
+    const sass = require('gulp-sass')
+    const cssnext = require('postcss-cssnext')
+    const postcss = require('gulp-postcss')
+    const processors = [cssnext({
+        browsers: ['last 2 version']
+    })]
 
-  return gulp
-    .src('./scss/*.scss')
-    .pipe(sass())
-    .pipe(postcss(processors))
-    .pipe(gulp.dest('css/'))
+    return gulp
+        .src('./scss/*.scss')
+        .pipe(sass())
+        .pipe(postcss(processors))
+        .pipe(gulp.dest('css/'))
 });
 
 gulp.task('serve', done => {
-  browserSync.init({
-    server: {
-      baseDir: './',
-      index: 'index.html',
-    },
-  })
-  done()
+    browserSync.init({
+        server: {
+            baseDir: './',
+            index: 'index.html',
+        },
+    })
+    done()
 })
 
 gulp.task('watch', () => {
-  const browserReload = done => {
-    browserSync.reload()
-    done()
-  }
-  gulp.watch('./dist/**/*', browserReload);
-  gulp.watch('./*.html', browserReload);
-  gulp.watch('./scss/**/*', gulp.series('scss'));
+    const browserReload = done => {
+        browserSync.reload()
+        done()
+    }
+    gulp.watch('./**/*', browserReload);
+    gulp.watch('./*.html', browserReload);
+    gulp.watch('./scss/**/*', gulp.series('scss'));
 })
 
 gulp.task('default', gulp.series('serve', 'watch'))
